38ba0473561fd11b31e2a0bd6c7bc999a7a200e6
Haseeb Rehan <haseebrehan@yahoo.com>
2021-08-06 23:34:44 -0500

    new name of resturant

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -40,7 +40,7 @@ class Header extends Component{
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav}/>
                             <NavbarBrand className="mr-auto" href="/">
-                                <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion"/>
+                                <img src="assets/images/logo.png" height="30" width="41" alt="Khan Baba Resturant"/>
                             </NavbarBrand>
 
                             <Collapse isOpen={this.state.isNavOpen} navbar>
@@ -70,7 +70,7 @@ class Header extends Component{
                     <div className="container">
                         <div clasName="row row-header">
                             <div className="col-12 col-sm-6">
-                                <h1> Ristorante Con Fusion</h1>
+                                <h1> Khan Baba Resturant</h1>
                                 <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
