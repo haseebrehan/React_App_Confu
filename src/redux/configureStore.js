d803fa856dc2651b37e68de7f012a9e769e36b04
Haseeb Rehan <haseebrehan@yahoo.com>
2021-08-07 20:39:02 -0500

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-//import { Reducer, initialState } from './reducer'
+import {createForms} from 'react-redux-form';
 import {Dishes} from './dishes';
 import {Comments} from './comments';
 import {Promotions} from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'
+import logger from 'redux-logger';
+import {InitialFeedback} from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -14,7 +15,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             leaders : Leaders,
-            promotions : Promotions
+            promotions : Promotions,
+            ...createForms({
+                feedback:InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
