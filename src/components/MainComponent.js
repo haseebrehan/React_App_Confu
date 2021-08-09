675fbcc1d0a890454ecb66f6cd328881bdf962e9
Haseeb Rehan <haseebrehan@yahoo.com>
2021-08-09 18:58:50 -0500

    Task 2 Assignmnet 4

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
-import {postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import {postFeedback,postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import {actions} from 'react-redux-form'
 
 const mapStateToProps = state => {
@@ -27,7 +27,9 @@ const mapStateToProps = state => {
     resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos()),
-    fetchLeaders: () => dispatch(fetchLeaders())
+    fetchLeaders: () => dispatch(fetchLeaders()),
+    postFeedback: (firstname,lastname,telnum,email,agree,contactType,message) =>
+      dispatch(postFeedback(firstname,lastname,telnum,email,agree,contactType,message))
   });
 
 class Main extends Component {
@@ -80,7 +82,11 @@ class Main extends Component {
                 <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                 <Route path='/menu/:dishId' component={DishWithId}/>
                 <Route path='/aboutus' component={() => <About leaders={this.props.leaders}/> }/>
-                <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
+                <Route exact path='/contactus' component={() => 
+                <Contact 
+                  resetFeedbackForm={this.props.resetFeedbackForm}
+                  postFeedback={this.props.postFeedback}
+                  />} />
                 <Redirect to='/home' />
                 </Switch>
                 <Footer/>
