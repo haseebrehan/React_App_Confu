30466e6a8d4a8380ce4f41231bf90a2fbec596c8
Haseeb Rehan <haseebrehan@yahoo.com>
2021-08-08 23:23:57 -0500

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
     this.toggleModal();
     //console.log("Current State is: " + JSON.stringify(values));
     //alert("Current State is: " + JSON.stringify(values));
-    this.props.addComment(this.props.dishId,values.rating,values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     
   }
 
@@ -133,7 +133,7 @@ class CommentForm extends Component {
    }
 
 
-  function RenderComments({comments, addComment, dishId}) {
+  function RenderComments({comments, postComment, dishId}) {
     if (comments != null)       
             return(
                     <div className="col-12 col-md-5">
@@ -148,7 +148,7 @@ class CommentForm extends Component {
                                 );
                             })}
                         </ul>
-                        <CommentForm dishId={dishId} addComment={addComment} />
+                        <CommentForm dishId={dishId} postComment={postComment} />
                     </div>
                  );
     else
@@ -197,7 +197,7 @@ class CommentForm extends Component {
                         <RenderDish dish={props.dish}/>
                       
                         <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}/> 
                         
                         </div>
